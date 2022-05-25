870d70ef468afe0167c5de58c308f29b1d769180
RubenNair <ruben.k.nair@gmail.com>
2022-05-25 16:31:58 +0200

    Add random agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,2 +1,3 @@
 from .opt_cmdp import OptCMDPAgent
 from .abs_opt_cmdp import AbsOptCMDPAgent
+from .random_agent import RandomAgent
--- scripts/cliff_walking.py
+++ scripts/cliff_walking.py
@@ -2,30 +2,35 @@ import os
 
 from agents import OptCMDPAgent
 from agents import AbsOptCMDPAgent
+from agents import RandomAgent
 from util.training import run_experiments_batch
 
 
 def main():
     c = 2
     h = 15
-    number_of_episodes = 5000
-    seeds = range(100)
-    out_dir = os.path.join('results', os.path.basename(__file__).split('.')[0])
+    number_of_episodes = 200
+    seeds = range(3)
+    out_dir = os.path.join('results', os.path.basename(__file__).split('.')[0] + "4(3seeds200episodes)")
     eval_episodes = 1000
     env_id = "gym_factored:cliff_walking_cost-v0"
     env_kwargs = {"num_rows": 4, "num_cols": 6}
 
     agents = [
+        ('Random', RandomAgent, {
+            'cost_bound': c,
+            'horizon': h
+        }),
         ('OptCMDP', OptCMDPAgent, {
             'cost_bound': c,
             'horizon': h
         }),
-        ('AbsOptCMDP ground', AbsOptCMDPAgent, {
-            'features': [0, 1],
-            'cost_bound': c,
-            'horizon': h,
-            'policy_type': 'ground'
-        }),
+        # ('AbsOptCMDP ground', AbsOptCMDPAgent, {
+        #     'features': [0, 1],
+        #     'cost_bound': c,
+        #     'horizon': h,
+        #     'policy_type': 'ground'
+        # }),
         # ('AbsOptCMDP abs', AbsOptCMDP, {
         #     'features': [0, 1],
         #     'cost_bound': c,
